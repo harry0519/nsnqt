3f359aa5a0cb9a056abe067a5abf62d7dfc04f14
wangxian111 <wangxian.111@163.com>
2016-10-30 22:17:39 +0800

    fields.py update

--- nsnqtlib/db/fields.py
+++ nsnqtlib/db/fields.py
@@ -5,9 +5,5 @@ par_list_stock  = "pre_close","open","high","low","close","volume","amt","dealnu
 par_list_future = "pre_close","open","high","low","close","volume","amt","oi"
 par_list_fund   = "pre_close","open","high","low","close","volume","amt"
 
-FIELD_SECURITY_GENERAL = "sec_code", "sec_name","close_price","total_market_value",
-                            "mkt_cap_float","trade_status","last_trade_day","ipo_day","province",
-                            "sec_type","listing_board","exchange"
-FIELD_COMPANY_GENERAL = "company_name","inception_date","ipo_date","outstanding_shares","sec_type",
-                            "regcapital","chairman","discloser","address","office","zipcode",
-                            "telephone","fax","website","comp_name_eng"
+FIELD_SECURITY_GENERAL = "sec_code", "sec_name","close_price","total_market_value","mkt_cap_float","trade_status","last_trade_day","ipo_day","province","sec_type","listing_board","exchange"
+FIELD_COMPANY_GENERAL = "company_name","inception_date","ipo_date","outstanding_shares","sec_type","regcapital","chairman","discloser","address","office","zipcode", "telephone","fax","website","comp_name_eng"
