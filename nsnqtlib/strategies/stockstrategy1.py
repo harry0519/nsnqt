5771a0e9cbc09839d90fcc8e1a0bcf6511ca8436
xuhshen <04yylxsxh@163.com>
2016-11-16 22:59:16 +0800

    refactor

--- nsnqtlib/strategies/stockstrategy1.py
+++ nsnqtlib/strategies/stockstrategy1.py
@@ -3,10 +3,10 @@ from  nsnqtlib.db.mongodb import MongoDB
 import threading
 import time
 import datetime
-import math
+import sys
+import pandas as pd
 DATES = []
 HOLDS=[]
-RESULTS=[]
 
 class strategy1(object):
     '''
@@ -23,15 +23,62 @@ class strategy1(object):
         m_vol = sum(data)/len(data)
         return m_vol
     
-    def max_price(self):
-        pass
+    def buy_condition(self,vol,vol_data,close,last_high,maxprice,minprice,count,vol_weight=1.2):
+        if  self.condition1(vol,vol_data,vol_weight) and \
+            self.condition2(close,last_high) and \
+            self.condition3(close,maxprice) and \
+            self.condition4(close,minprice): 
+            return True
+        return False
     
-    def min_price(self):
-        pass
+    def sell_conditon(self,buy_price,currentday_high,currentday_low,hold_days,gain_grads=0.1,loss_grads=-0.05,dayout=10):
+        if self.stopgain_condition(buy_price,currentday_high,gain_grads):
+            return True,"stopgain"
+        elif self.stoploss_condition(buy_price,currentday_low,loss_grads):
+            return True,"stoploss"
+        elif self.holdingtime_condition(hold_days,dayout):
+            return True,"holdtime"
+        return False,None
+    
+    def stopgain_condition(self,buy_price,current_price,grads=0.1):
+        if (current_price-buy_price)/buy_price >= grads:
+            return True
+        return False
+    
+    def stoploss_condition(self,buy_price,current_price,grads=-0.05):
+        if (current_price-buy_price)/buy_price <= grads:
+            return True
+        return False
+    
+    def holdingtime_condition(self,hold_days,dayout=10):
+        if hold_days >= dayout:
+            return True
+        return False
+    
+    def condition1(self,vol,vol_data,vol_weight=1.2):
+        if vol >= vol_weight * self.mean_volume(vol_data):
+            return True
+        return False
+    
+    def condition2(self,close,last_high):
+        if close >= last_high:
+            return True
+        return False
+    
+    def condition3(self,close,high,grads=0.2):
+        if (high-close)/high >= grads:
+            return True
+        return False
+    
+    def condition4(self,close,low,grads=0.05):
+        if (close-low)/low <= grads:
+            return True
+        return False
     
     def histofyreturn(self,db="ml_security_table",table=""):
         buy = []
-        vol_a = 1.2
+        stopwin = 0.1
+        stoploss = -0.1
         vol_day = 10
         price_day = 60
         df = self._getdata(collection=table)
@@ -39,63 +86,63 @@ class strategy1(object):
         origindata=[]
         lst = [l for l in df[["date","volume","close","high","low","open","pre_close"]].fillna(0).values if l[1] !=0]
         for line in lst[count:]:
-            if line[1] == 0:continue
+            vol = line[1]
+            if vol == 0:continue
+            close = line[2]
+            last_high = lst[count-1][3]
             vol_data = [i[1] for i in lst[count-vol_day:count]]
             maxprice = max([i[3]] for i in lst[count-price_day:count])[0]
             minprice = min([i[4]] for i in lst[count-price_day:count])[0]
-            for b in buy:
+            
+            for b in buy[:]:
                 d=b[0]
                 c=b[1]
-                if (line[3]-d[2])/d[2]>=0.1:
+                buy_date = d[0]
+                sell_date = line[0]
+                hold_days = count-c
+                buy_price = d[2]
+                currentday_high = line[3]
+                currentday_low = line[4]
+                
+                is_sell,selltype = self.sell_conditon(buy_price,currentday_high,currentday_low,\
+                                      hold_days,gain_grads=0.1,\
+                                      loss_grads=-0.05,dayout=10)
+                if is_sell:
                     buy.remove(b)
-                    print (0.1)
-#                     print (0.1)
-                    RESULTS.append(0.1)
-                elif count-c >=10:
-                    buy.remove(b)
-                    print ((line[2]-d[2])/d[2])
-                elif (line[4]-d[2])/d[2]<=-0.05:
-                    buy.remove(b)
-                    print (-0.05)
+                    
+                    if selltype == "stopgain": 
+                        profit = stopwin
+                    elif selltype == "stopgain":
+                        profit = stoploss
+                    else: 
+                        profit = (close-buy_price)/buy_price
+                    origindata.append([table,buy_date,sell_date,hold_days,profit])
+                    print (profit)
             
-            if line[1] >= vol_a * self.mean_volume(vol_data)and line[2]>=lst[count-1][3] \
-            and (maxprice-line[2])/maxprice >=0.2 and (line[2]-minprice)/minprice<=0.05:
+            if self.buy_condition(vol,vol_data,close,last_high,maxprice,minprice,count,vol_weight=1.2):
                 buy.append((line,count))
             count +=1
+        return origindata   
             
-#         print (table)
-#         print (holding)
-            
-    def getnewindex(self,datas,newdata,l,base=0):
-        '''datas:should be sorted from min to max,
-           newdats: the data need to check the position in the sorted datas
-           l:len of data
-           base:first position of the datas in origin datas
-        '''
-        index = int(l/2)
-        if datas[index] > newdata:
-            if l==1:return base
-            d = datas[:index]
-            nl = index
-            return self.getnewindex(d,newdata,nl,base)
-        elif datas[index] < newdata:
-            if l<=2:return base+l
-            d = datas[index+1:] 
-            nl = int((l-1)/2)
-            return self.getnewindex(d,newdata,nl,base+index+1)
-        elif datas[index] == newdata:
-            return base+index    
-    
-            
+
+result = []            
+stop=0
 s=strategy1()
 stocklist = s.m.getallcollections("ml_security_table")
-# s.histofyreturn(table="600455.SH")
+# s.his1tofyreturn(table="600455.SH")
+
 for i in stocklist:
 #     print (i)  
     try:
-        s.histofyreturn(table=i)
+        r = s.histofyreturn(table=i)
+        if r:result.extend(r)
     except:
         pass
+#     if stop >=100:
+#         print (result)
+df = pd.DataFrame(result,columns=["stock","buy_date","sell_date","holddays","profit"])
+df.to_csv("test1.csv")
+#         sys.exit()
     
 # 
 # print (len(set(DATES)))
