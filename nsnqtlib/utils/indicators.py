853be22800fcee6b1e132aad7f3d3829e051d69d
xuhshen <04yylxsxh@163.com>
2017-01-02 14:25:47 +0800

    add indicators

--- nsnqtlib/utils/indicators.py
+++ nsnqtlib/utils/indicators.py
@@ -1,11 +1,115 @@
 # -*- coding:utf-8 -*-
 import random
+from nsnqtlib.config import DB_SERVER,DB_PORT,USER,PWD,AUTHDBNAME
+from  nsnqtlib.db.mongodb import MongoDB
+import pandas as pd
+import datetime
 class StockIndicator(object):
     '''
     various kinds of stock indicator method
     '''
-    def __init__(self,):
+    def __init__(self,startdate=(2011, 1, 1)):
+        self.m = MongoDB(DB_SERVER,DB_PORT,USER,PWD,AUTHDBNAME)
+        self.formatlist = ["date","volume","close","high","low","open","pre_close"]
+        self.startdate = datetime.datetime( *startdate,0,0,0,0)
         pass
+    
+    
+    def setenv(self,collection):
+        self.count = 0
+        self.indictors = []
+        self.collection = collection
+        data = self._getdata(collection)
+        self.datalst = [l for l in data[self.formatlist].fillna(0).values if l[1] !=0]
+        self.datalst = self.rehabilitation(self.datalst)
+        pass
+    
+    def setlooplist(self,lst=[]):
+        if not lst:
+            self.looplist = self.m.getallcollections("ml_security_table")
+        else:
+            self.looplist = lst
+        return self.looplist
+    
+    def _getdata(self,collection="600455.SH",db="ml_security_table",out=[],isfilt=True,filt={}):
+        if not out:out = self.formatlist
+        if isfilt and not filt: filt = {"date":{"$gt": self.startdate}}
+        query = self.m.read_data(db,collection,filt=filt)
+        return self.formatquery(query,out)
+    
+    def formatquery(self,query,out):
+        '''
+        query:your source data ,should be a list with dict
+        out:the fields you want to convert into dataframe 
+        '''
+        if not out:
+            query = [i for i in query.sort("date", 1)]
+        else:
+            query = [{k:i[k] for k in out} for i in query.sort("date", 1)]
+        return pd.DataFrame(query)
+    
+    def rehabilitation(self,lst):
+        close = lst[0][2]
+        c = 1
+        reh = []
+        for line in lst[1:]:
+            c_preclose = line[6]
+            if c_preclose != close:
+                reh = [[i[0],i[1]*c_preclose/close] for i in reh]
+                reh.append([c,c_preclose/close])
+            close = line[2]
+            c += 1
+        result = []
+        sc = 0
+        for idx in range(len(reh)):
+            weight = reh[idx][1]
+            ec = reh[idx][0]
+            piece = self.recount(lst,sc,ec,weight)
+            result.extend(piece)
+            sc = ec
+        result.extend(self.recount(lst,ec,-1,1))
+        return result
+    
+    def recount(self,lst,sc,ec,weight):
+        rst = []
+        for line in lst[sc:ec]:
+            rst.append([line[0],line[1],*[i*weight for i in line [2:]]])
+        return rst     
+    
+    def getallindictorperday(self,indictors=[]):
+        '''input: 
+             indictors :should be a list of list,example:[(name,func),(name,func)]
+        '''
+        indicts={}
+        for i in indictors:
+            indicts[i[0]] = i[1]()
+        return indicts
+    
+    
+    def setmacdlist(self,lst):
+        s_ema = f_ema = lst[0][2]
+        dem = 0
+        self.difflist = []
+        self.demlist = []
+        self.macdlist = []
+        self.s_ema = []
+        self.f_ema = []
+        
+        for line in lst:
+            s_ema = (s_ema*(self.emaslow-1)+ 2*line[2])/(self.emaslow+1)
+            f_ema = (f_ema*(self.emafast-1)+ 2*line[2])/(self.emafast+1)
+            dif = f_ema-s_ema
+            self.difflist.append(dif)
+            self.s_ema.append(s_ema)
+            self.f_ema.append(f_ema)
+           
+            dem = (dem*(self.demday-1)+ 2*dif)/(self.demday+1)
+            self.demlist.append(dem)
+            self.macdlist.append(2*(dif-dem))
+    
+    
+    
+    
     
     def islowestvolume(self,data,section=60):
         if (len(data) >= section) and (data[0] == min(data[0:section])):
