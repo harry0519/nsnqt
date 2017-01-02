164a81267f3e2d4b0ff46fd0cf8f2ba951c3bfc9
Xu <jian-stuart.xu@nokia.com>
2017-01-02 17:45:39 +0800

    MACD indicators methods add un-tested

--- nsnqtlib/utils/indicators.py
+++ nsnqtlib/utils/indicators.py
@@ -12,6 +12,9 @@ class StockIndicator(object):
         self.m = MongoDB(DB_SERVER,DB_PORT,USER,PWD,AUTHDBNAME)
         self.formatlist = ["date","volume","close","high","low","open","pre_close"]
         self.startdate = datetime.datetime( *startdate,0,0,0,0)
+        self.emaslow  = 26
+        self.emafast = 12
+        self.demday = 9
         pass
     
     
@@ -106,9 +109,98 @@ class StockIndicator(object):
             dem = (dem*(self.demday-1)+ 2*dif)/(self.demday+1)
             self.demlist.append(dem)
             self.macdlist.append(2*(dif-dem))
+        
+        return self.macdlist
     
+    #MACD related indicators
+    #Moving average: there will be unstable period in the beginning
     
+    #List of all indicators with completed "close" list
+    def EMA_list(self, lst, timeperiod=10):
+        ema = []
+        current = lst[0][2]
+        for i in lst:
+            current = (current*(timeperiod-1)+ 2*i[2])/(timeperiod+1)
+            ema.append(current)
+        return ema
+                
+    def DIF_list(self, lst, fastperiod=12, slowperiod=26):
+        dif = []
+        dif_t = []
+        s_ema = self.EMA_list(lst, slowperiod)
+        f_ema = self.EMA_list(lst, fastperiod)
+        for i in range(len(lst)):
+            current = f_ema[i]-s_ema[i]
+            dif.append(current)
+            dif_t.append([0,0,current,0])
+        return dif, dif_t
+        
+    def DEA_list(self, lst, fastperiod=12, slowperiod=26, signalperiod=9):
+        _,dif = self.DIF_list(lst,fastperiod,slowperiod)
+        return self.EMA_list(dif, signalperiod)
+        
+    def MACD_list(self, lst, fastperiod=12, slowperiod=26, signalperiod=9):
+        macd = []
+        dif,dif_t = self.DIF_list(lst,fastperiod,slowperiod)
+        dea = self.EMA_list(dif_t, signalperiod)
+        for i in range(len(lst)):
+            macd.append(2*(dif[i]-dea[i]))
+        return macd
+
+    #updating value with previous and current value
+    '''
+    input: 
+        history: Sorted list by date: 
+                 [["date","EMA12","EMA26","DIF","DEA","MACD"],[...],...]
+                 ! Last one should be yesterday's data
+        current: float: Todays' close price
+    output:
+        current indicator value.
+    '''
+    def EMA(self, last, current, timeperiod = 10):
+        return (current*(timeperiod-1)+ 2*last)/(timeperiod+1)
     
+    def EMA12(self, history, current):
+        return self.EMA(history[-1][1], current, 12)
+        
+    def EMA26(self, history, current):
+        return self.EMA(history[-1][2], current, 26)
+    
+    def DIF(self, history, current):
+        return (self.EMA12(history, current)-self.EMA26(history, current))
+    
+    def DEA(self, history, current):
+        return self.EMA(history[-1][3], self.DIF(history, current), 9)
+    
+    def MACD(self, history, current):
+        return 2*(self.DIF(history, current)-self.DEA(history, current))
+    
+    '''
+    input: 
+        history: Sorted list by date: 
+                 [["date","EMA12","EMA26","DIF","DEA","MACD"],[...],...]
+                 ! Last one should be last available data
+        current: [["date","close"],...]
+                 Earliest day which is not updated with indicators to todays' close price
+    output:
+        Indicator value list in date order:
+            [["date","EMA12","EMA26","DIF","DEA","MACD"],[...],...]
+    '''
+    def MACD_update(self, history, current):
+        '''
+        Update MACD related all values: inc. EMA12, EMA26, DIF, DEA, MACD
+        '''
+        #To be added: update them into database
+        updates = []
+        indicators = history[-1]
+        for i in range(len(current)):
+            indicators = [current[i][0],self.EMA12([indicators], current[i][1]), \
+                self.EMA26([indicators], current[i][1]), \
+                self.DIF([indicators], current[i][1]), \
+                self.DEA([indicators], current[i][1]), \
+                self.MACD([indicators], current[i][1])]
+            updates.append(indicators)
+        return updates
     
     
     def islowestvolume(self,data,section=60):
@@ -141,13 +233,19 @@ class StockIndicator(object):
     
 if __name__ == '__main__':
     s= StockIndicator()
+    '''
     data=random.sample(range(100),100)
     print(data)
     datas=sorted(data[:10])
     print (datas)
     for i in data:
         print (s.getnewindex(datas,i,10))
-    
+    '''
+    a = [[0,1,3],[0,1,5],[0,1,110],[0,1,98],[0,1,35],[0,1,75],[0,1,30],[0,1,16],[0,1,8],\
+         [0,1,9],[0,1,94],[0,1,13],[0,1,18],[0,1,175],[0,1,565],[0,1,511],[0,1,543],[0,1,531],\
+         [0,1,115],[0,1,4],[0,1,5],[0,1,23],[0,1,19],[0,1,18],[0,1,15],[0,1,43],[0,1,6]]
+    print(s.setmacdlist(a))
+    print(s.MACD_list(a))
     
     
     
